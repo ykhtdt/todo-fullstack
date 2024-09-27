44e0c9d84108a07d418a4c6da2fb86108199974b
박종광 <jgpark7281@gmail.com>
2024-09-27 16:02:56 +0900

    feat(backend): update signup api

--- apps/backend/src/index.ts
+++ apps/backend/src/index.ts
@@ -1,4 +1,5 @@
 import { serve } from "@hono/node-server"
+import bcrypt from "bcrypt"
 import { Hono } from "hono"
 import { cors } from "hono/cors"
 import db from "~/model/db"
@@ -20,18 +21,29 @@ app.get("/", (c) => {
 
 app.post("/signup", async (c) => {
   const body = await c.req.json()
-  console.log(body)
 
-  const users = await db.query(
-    "SELECT * FROM users"
-  )
+  const { email, password } = body
 
-  console.log(users);
+  const existingUser = await db.query("SELECT * FROM users WHERE email = $1", [email])
+
+  if (existingUser.rowCount && existingUser.rowCount > 0) {
+    return c.json({
+      ok: false,
+      message: "This email is already registered.",
+    }, 409)
+  }
+
+  const hashedPassword = await bcrypt.hash(password, 10)
+
+  const query = "INSERT INTO users (email, password) VALUES ($1, $2)";
+  const values = [email, hashedPassword];
+
+  await db.query(query, values);
 
   return c.json({
     ok: true,
-    message: "Test"
-  })
+    message: "Your account has been created successfully.",
+  }, 201)
 })
 
 const port = 3002
